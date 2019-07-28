0598b896726650e37cdb6b2d0f5f7b978d6a7dcf
umbroom <omori.fuki@gmail.com>
2019-07-28 23:28:31 +0900

    fix picture natural-color

--- script/main.js
+++ script/main.js
@@ -1,20 +1,28 @@
 var contents = document.getElementsByClassName('content');
+var wrappers = document.getElementsByClassName('wrapper');
+var blocks = document.getElementsByClassName('block');
 
-window.onload = function(){
-    document.getElementsByClassName('header')[0].classList.add('active');
+function check_active(){
     for(var i=0; i<contents.length; i++){
         let content = contents[i];
-        if(window.innerHeight*3/4 > content.getBoundingClientRect().top){
+        if(window.innerHeight > content.getBoundingClientRect().top){
             content.classList.add('active');
+        }
+    }
+    for(var i=0; i<wrappers.length; i++){
+        let wrapper = wrappers[i];
+        if(window.innerHeight > wrapper.getBoundingClientRect().top){
+            wrapper.classList.add('active');
+        }
+    }
+    for(var i=0; i<blocks.length; i++){
+        let block = blocks[i];
+        if(window.innerHeight > block.getBoundingClientRect().top){
+            block.classList.add('active');
         }
     }
 }
 
-window.onscroll = function(){
-    for(var i=0; i<contents.length; i++){
-        let content = contents[i];
-        if(window.innerHeight*3/4 > content.getBoundingClientRect().top){
-            content.classList.add('active');
-        }
-    }
-}
+window.onload = check_active;
+
+window.onscroll = check_active;
