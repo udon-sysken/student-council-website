750b8ef902590575e747659781cb14082ffd904b
umbroom <32835505+umbroom@users.noreply.github.com>
2019-07-29 17:19:25 +0900

    add menu-bar

--- script/main.js
+++ script/main.js
@@ -2,6 +2,18 @@ var contents = document.getElementsByClassName('content');
 var wrappers = document.getElementsByClassName('wrapper');
 var blocks = document.getElementsByClassName('block');
 
+function open_menu(){
+    document.getElementsByClassName('header')[0].classList.add('open');
+}
+
+function close_menu(){
+    document.getElementsByClassName('header')[0].classList.remove('open');
+}
+
+function content_scroll(class_name){
+    window.scrollTo(0, document.getElementsByClassName(class_name)[0].getBoundingClientRect().top);
+}
+
 function check_active(){
     for(var i=0; i<contents.length; i++){
         let content = contents[i];
@@ -24,5 +36,5 @@ function check_active(){
 }
 
 window.onload = check_active;
-
 window.onscroll = check_active;
+window.onresize = check_active;
